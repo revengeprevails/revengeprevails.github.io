3ef96d757e72c2758c45cd8996375308d84b426d
isabellaramare <isabella.ramare@hotmail.com>
2020-12-02 11:47:16 +0100

    Updates

---
+++ precache-manifest.1b13b4bddc8782e54eadde005fe89245.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3916feadad618e412eaa2cf8dd2b02ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a78b40554125dc570ebd",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "6630c954e7aeba7deccd",
+    "url": "/static/css/main.bbf423ec.chunk.css"
+  },
+  {
+    "revision": "a78b40554125dc570ebd",
+    "url": "/static/js/2.03538399.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.03538399.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91dc84332b23d2cfdde4",
+    "url": "/static/js/3.9f560126.chunk.js"
+  },
+  {
+    "revision": "6630c954e7aeba7deccd",
+    "url": "/static/js/main.8266c5b0.chunk.js"
+  },
+  {
+    "revision": "90584bd842fd8a0375e4",
+    "url": "/static/js/runtime-main.0aa52724.js"
+  },
+  {
+    "revision": "3c7962a1fc2a0d2a60fb0271b819e8b6",
+    "url": "/static/media/bakgrund mörk.3c7962a1.png"
+  },
+  {
+    "revision": "2e0f9d035578bb0d5218d3c5a580d2e0",
+    "url": "/static/media/bakgrund.2e0f9d03.jpg"
+  },
+  {
+    "revision": "f23c7f485d4e877b0d0800a4a4428597",
+    "url": "/static/media/bandbild 3 2020 bw.f23c7f48.png"
+  },
+  {
+    "revision": "a2293659e43addec6798dfa1551fec26",
+    "url": "/static/media/corner snake gray.a2293659.svg"
+  },
+  {
+    "revision": "6565702f1158c1c1b0ce4dcd32dae19b",
+    "url": "/static/media/corner snake red.6565702f.svg"
+  },
+  {
+    "revision": "671944038083e82f1fe5028eb3afe274",
+    "url": "/static/media/habitats.67194403.png"
+  },
+  {
+    "revision": "787ced4df7c2657174de22701c68ac62",
+    "url": "/static/media/rp logo.787ced4d.svg"
+  }
+]);
