e2dcdcb03fd820cb6bfbfcd3f69feaa8a5a509f5
isabellaramare <isabella.ramare@hotmail.com>
2020-12-09 19:27:42 +0100

    Updates

---
+++ precache-manifest.12fcf13bf8bcbd504f784bc0a04bca3a.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341a1be50bd7d0c45f3525c916a77c38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16db9577f8ad780543af",
+    "url": "/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "bf7a4136d4d2cf5d9aea",
+    "url": "/static/css/main.cac80b26.chunk.css"
+  },
+  {
+    "revision": "16db9577f8ad780543af",
+    "url": "/static/js/2.3f6af2fb.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.3f6af2fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aef999fdf4616e730bb9",
+    "url": "/static/js/3.00c97c14.chunk.js"
+  },
+  {
+    "revision": "bf7a4136d4d2cf5d9aea",
+    "url": "/static/js/main.b795e719.chunk.js"
+  },
+  {
+    "revision": "5b7bbabd998f3205b98a",
+    "url": "/static/js/runtime-main.af8a3cb9.js"
+  },
+  {
+    "revision": "7b0ed5c4c626e2e05c601f3660844903",
+    "url": "/static/media/Christoffer small.7b0ed5c4.jpg"
+  },
+  {
+    "revision": "7dc351dfe36a8f789a8651bd4f050b3b",
+    "url": "/static/media/Isabella small.7dc351df.jpg"
+  },
+  {
+    "revision": "469ea690419f875f1e40ae0d3659df6a",
+    "url": "/static/media/Josef small.469ea690.jpg"
+  },
+  {
+    "revision": "f057c270a56f402b7cd6f4937d5f512b",
+    "url": "/static/media/Ludvig small.f057c270.jpg"
+  },
+  {
+    "revision": "3c7962a1fc2a0d2a60fb0271b819e8b6",
+    "url": "/static/media/bakgrund mörk.3c7962a1.png"
+  },
+  {
+    "revision": "2e0f9d035578bb0d5218d3c5a580d2e0",
+    "url": "/static/media/bakgrund.2e0f9d03.jpg"
+  },
+  {
+    "revision": "9952290252af90a81a7c0380d0d0bda4",
+    "url": "/static/media/bandbild 1 2020 bw.99522902.jpg"
+  },
+  {
+    "revision": "32c9bb890f48790904fbff7fc832b39b",
+    "url": "/static/media/bandbild 3 2020 bw.32c9bb89.jpg"
+  },
+  {
+    "revision": "71b1857669dc4210798745705a8483d1",
+    "url": "/static/media/black void injection.71b18576.png"
+  },
+  {
+    "revision": "a2293659e43addec6798dfa1551fec26",
+    "url": "/static/media/corner snake gray.a2293659.svg"
+  },
+  {
+    "revision": "6565702f1158c1c1b0ce4dcd32dae19b",
+    "url": "/static/media/corner snake red.6565702f.svg"
+  },
+  {
+    "revision": "36dd34e9ed9c1e1425acef5734980b9d",
+    "url": "/static/media/dark trembling solace.36dd34e9.png"
+  },
+  {
+    "revision": "5dd2782227e7b1a7784ec60cf2cd3cec",
+    "url": "/static/media/false hope cruelty.5dd27822.png"
+  },
+  {
+    "revision": "671944038083e82f1fe5028eb3afe274",
+    "url": "/static/media/habitats.67194403.png"
+  },
+  {
+    "revision": "afb9453d8be912cab3a01e88273f48a8",
+    "url": "/static/media/hand.afb9453d.svg"
+  },
+  {
+    "revision": "eecd815d7be079330ec110917905d838",
+    "url": "/static/media/rp logo round white.eecd815d.svg"
+  },
+  {
+    "revision": "787ced4df7c2657174de22701c68ac62",
+    "url": "/static/media/rp logo.787ced4d.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2735020eb7e573e9f833138430fb4a22.js"
+  "/precache-manifest.12fcf13bf8bcbd504f784bc0a04bca3a.js"
 );
 
 self.addEventListener('message', (event) => {
