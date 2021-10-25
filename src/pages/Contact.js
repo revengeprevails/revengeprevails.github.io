7e7c408c0bd3efcd32d0eedbaa2786f680c44714
Revenge Prevails <revengeprevails@gmail.com>
2021-10-25 14:56:33 +0200

    added some gigs

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -11,8 +11,8 @@ class Contact extends React.Component {
         <Row className="justify-content-center vh-100 pb-5">
           <Col className="my-auto col-md-6 col-12 contact-text">
             <h2 className="contact-text py-2">Do you wish to make contact?</h2>
-            <p className="contact-text">email to: <a className="my-link" target="_blank" href="mailto: revengeprevails@gmail.com">revengeprevails@gmail.com</a></p>
-            <p className="contact-text">or message us: <a className="my-link" target="_blank" href="https://www.facebook.com/revengeprevails">facebook.com/revengeprevails</a></p>
+            <p className="contact-text">email to: <a className="my-link" target="_blank" rel="noopener noreferrer" href="mailto: revengeprevails@gmail.com">revengeprevails@gmail.com</a></p>
+            <p className="contact-text">or message us: <a className="my-link" target="_blank" rel="noopener noreferrer" href="https://www.facebook.com/revengeprevails">facebook.com/revengeprevails</a></p>
           </Col>
           <Col className="my-auto col-md-6 col-12">
             <img className="w-100 mb-5" src={dodo}/>
